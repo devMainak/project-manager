55ae7471ec5b117795cd349cc06d005ba25bc88e
devMainak <mrterm404@gmail.com>
2025-05-02 22:12:47 +0530

    refactor: removed comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,9 +21,6 @@ initializeDatabase();
 
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/tasks", require("./routes/task.routes"));
-// app.use("/teams", require("./routes/team.routes"));
 app.use("/projects", require("./routes/project.routes"));
-// app.use("/tags", require("./routes/tag.routes"));
-// app.use("/report", require("./routes/report.routes"));
 
 module.exports = app;
