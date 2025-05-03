5d29d819cfe37c0278a099a1e0df5758e66fdacf
devMainak <mrterm404@gmail.com>
2025-05-03 12:26:36 +0530

    feat: deployed origin added

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const initializeDatabase = require("./config/db.config");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://gotask-frontend.vercel.app/",
   credentials: true,
 };
 
